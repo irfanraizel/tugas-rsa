81aba505a6c48f922401127d6b65d6cc15dbf3a6
Irfan <irfanraizel@gmail.com>
2024-12-03 23:26:46 +0700

    first commit

---
+++ function.php
@@ -0,0 +1,129 @@
+<?php
+// Fungsi untuk menghitung modulus eksponensial
+function mod_exp($base, $exp, $mod)
+{
+    $result = 1;
+    while ($exp > 0) {
+        if ($exp % 2 == 1) {
+            $result = ($result * $base) % $mod;
+        }
+        $base = ($base * $base) % $mod;
+        $exp = floor($exp / 2);
+    }
+    return $result;
+}
+
+// Fungsi untuk menghitung GCD (Greatest Common Divisor)
+function gcd($a, $b)
+{
+    while ($b != 0) {
+        $temp = $b;
+        $b = $a % $b;
+        $a = $temp;
+    }
+    return $a;
+}
+
+// Fungsi untuk mencari modulo invers
+function mod_inverse($e, $phi)
+{
+    $d = 0;
+    $x1 = 0;
+    $x2 = 1;
+    $y1 = 1;
+    $temp_phi = $phi;
+
+    while ($e > 0) {
+        $temp1 = floor($temp_phi / $e);
+        $temp2 = $temp_phi - $temp1 * $e;
+        $temp_phi = $e;
+        $e = $temp2;
+
+        $x = $x2 - $temp1 * $x1;
+        $y = $d - $temp1 * $y1;
+
+        $x2 = $x1;
+        $x1 = $x;
+        $d = $y1;
+        $y1 = $y;
+    }
+
+    if ($temp_phi == 1) {
+        return $d + $phi;
+    }
+
+    return null;
+}
+
+// Langkah 1: Key Generation
+function generate_keys($p, $q)
+{
+    $n = $p * $q; // n = p * q
+    $phi = ($p - 1) * ($q - 1); // φ(n) = (p-1)(q-1)
+
+    // Pilih e (1 < e < φ(n)) dan coprime dengan φ(n)
+    $e = 17;
+    while ($e < $phi) {
+        if (gcd($e, $phi) == 1) {
+            break;
+        }
+        $e++;
+    }
+
+    // Hitung d (invers modulo e terhadap φ(n))
+    $d = mod_inverse($e, $phi);
+
+    return [
+        'public' => ['e' => $e, 'n' => $n],
+        'private' => ['d' => $d, 'n' => $n],
+    ];
+}
+// Langkah 2: Enkripsi
+function encrypt($plaintext, $public_key)
+{
+    $e = $public_key['e'];
+    $n = $public_key['n'];
+
+    $cipher = [];
+    foreach (str_split($plaintext) as $char) {
+        $cipher[] = mod_exp(ord($char), $e, $n);
+    }
+    return $cipher;
+}
+
+// Langkah 3: Dekripsi
+function decrypt($ciphertext, $private_key)
+{
+    $d = $private_key['d'];
+    $n = $private_key['n'];
+
+    $plaintext = '';
+    foreach ($ciphertext as $char) {
+        $plaintext .= chr(mod_exp($char, $d, $n));
+    }
+    return $plaintext;
+}
+
+// Contoh penggunaan
+$p = 71; // Bilangan prima pertama
+$q = 67; // Bilangan prima kedua
+
+// Generate keys
+$keys = generate_keys($p, $q);
+$public_key = $keys['public'];
+$private_key = $keys['private'];
+
+echo "Public Key: " . json_encode($public_key) . "\n";
+echo "Private Key: " . json_encode($private_key) . "\n";
+
+// Plaintext
+$plaintext = "SBGH80166JHY78";
+echo "Plaintext: " . $plaintext . "\n";
+
+// Enkripsi
+$ciphertext = encrypt($plaintext, $public_key);
+echo "Ciphertext: " . json_encode($ciphertext) . "\n";
+
+// Dekripsi
+$decrypted_text = decrypt($ciphertext, $private_key);
+echo "Decrypted Text: " . $decrypted_text . "\n";
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+//include("function.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Implementasi RSA</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid d-flex justify-content-left">
+                <a class="navbar-brand d-flex align-items-center" href="#">
+                    <img src="img/ecom.jpg" alt="Logo" width="50" class="d-inline-block align-text-top">
+                    <h3>Toko Bangun Jaya</h3>
+                </a>
+            </div>
+        </nav>
+
+        <main>
+            <div class="d-flex justify-content-between row">
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+
+                <div class="card my-5" style="width: 18rem;">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134201-23030-uo7uux4ynbov38" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kabel NYM</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repellat nobis sed deleniti quae dicta, ab dignissimos tempora, earum, excepturi exercitationem saepe nisi reprehenderit quia placeat adipisci.</p>
+                        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
